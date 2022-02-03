cdffb9835d8a9b3e84efb361e24d1e1e715d02b3
yogidi <63420198+yogidi@users.noreply.github.com>
2022-02-03 21:41:21 +0530

    inital commit of maths.py

---
+++ maths.py
@@ -0,0 +1,22 @@
+# add implementation
+def add(x,y):
+    return x+y    #on Bug123
+
+#subtract implementation
+def subtract(x,y):
+    return x-y   #on master
+
+# multiply implementation
+def multiple(x,y):
+    return x*y   #on Bug456
+
+# divide implementation
+def divide(x,y):
+
+    return x/y #on master
+
+    if y==0:
+	return Divide_by_0_error #on bug789
+    else:
+	return x/y 
+
